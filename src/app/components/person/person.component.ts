7eddaa663b8d492cdb8849cfb5ebb0778dc2a77c
Salih Çetin <salihcetinteam@gmail.com>
2022-08-17 10:33:49 +0300

    List Persons

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,6 +1,7 @@
 import { PersonResponseModel } from './../../models/personResponseModel';
 import { Person } from './../../models/person';
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-person',
@@ -10,12 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class PersonComponent implements OnInit {
 
   persons:Person[]=[];
+  apiUrl = "http://localhost:8080/public/person?page=0&size=20"
+  body:{} = {
+    
+  }
+
     //personResponseModel:PersonResponseModel={};
   
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
-  ngOnInit(): void {0
+  ngOnInit(): void {
+    this.getPersons();
+  }
+
+  getPersons(){
+    this.httpClient.post<PersonResponseModel>(this.apiUrl,this.body).subscribe((response) => {
+      this.persons = response.content
+    });
   }
 
 }
