2cafe880577d5273dfffc91545305094b28a6661
Salih Çetin <salihcetinteam@gmail.com>
2022-08-17 10:02:51 +0300

    Update Person Component

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,3 +1,5 @@
+import { PersonResponseModel } from './../../models/personResponseModel';
+import { Person } from './../../models/person';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,48 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonComponent implements OnInit {
 
-  person1={
-    personId: 1,
-    personName: "Salih",
-    categoryId: 5,
-    age: 24
-  }
-  person2={
-    personId: 2,
-    personName: "Serdar",
-    categoryId: 12,
-    age: 28
-  }
-  person3={
-    personId: 3,
-    personName: "Levent",
-    categoryId: 55,
-    age: 30
-  }
-  person4={
-    personId: 4,
-    personName: "Kerim",
-    categoryId: 21,
-    age: 24
-  }
-  person5={
-    personId: 6,
-    personName: "Akın",
-    categoryId: 13,
-    age: 23
-  }
-
-  persons=[
-    this.person1,
-    this.person2,
-    this.person3,
-    this.person4,
-    this.person5
-  ]
+  persons:Person[]=[];
+    //personResponseModel:PersonResponseModel={};
+  
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {0
   }
 
 }
---
+++ src/app/models/person.ts
@@ -0,0 +1,11 @@
+export interface Person{
+    id:number;
+    nameSurname:string;
+    gender:string;
+    age:string;
+    educationStatus:string;
+    city:string;
+    district:string;
+    militaryStatus:string;
+
+}
